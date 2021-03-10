d475ad69daef89bd984be1f9284d12abf40b9b2b
Ivan MSI <arigatory@gmail.com>
2021-03-11 00:05:43 +0300

    refactored

---
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,22 @@
+﻿using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        public DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Date = request.Date,
+                Email = request.Email
+            };
+        }
+    }
+}
