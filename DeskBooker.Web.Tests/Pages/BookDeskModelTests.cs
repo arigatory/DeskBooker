26fc8a225a9e629e5f41013f1228f84fe316e17a
Ivan MSI <arigatory@gmail.com>
2022-01-04 18:05:58 +0300

    tdd for booking on post

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -1,7 +1,33 @@
-﻿namespace DeskBooker.Web.Pages
+﻿using Xunit;
+using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using Moq;
+
+namespace DeskBooker.Web.Pages
 {
     public class BookDeskModelTests
     {
+        [Fact]
+        public void ShouldCallDeskMethodOfProcessor()
+        {
+            // Arrange 
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+
+
+            // Act 
+            bookDeskModel.OnPost();
+
+
+            // Assert
+            processorMock.Verify(x => x.BookDesk(bookDeskModel.DeskBookingRequest), Times.Once);
+        }
+
+
 
     }
 }
